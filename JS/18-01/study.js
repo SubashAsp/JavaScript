6f571a29a8502af52a5d65f58fd176880d604bd2
Subash Thiru <subash.thiru@aspiresys.com>
2025-01-20 11:43:20 +0530

    updates

---
+++ JS/18-01/study.js
@@ -0,0 +1,36 @@
+// shallow copy
+// const original = {
+//     name: 'John',
+//     age:30,
+//     address: {
+//       city: 'New York',
+//       country: 'USA'
+//     }
+//   };
+   
+//   const shallowCopy = original;
+//   shallowCopy.name = 'subash';
+//   console.log(original.name);
+//   console.log(shallowCopy.name);
+
+//   const original2 = {
+//     detail: {name: 'John',
+//     age: 30
+//     },
+//     address: {
+//       city: 'New York',
+//       country: 'USA'
+//     }
+//   };
+   
+//   const shallowCopy2 = {...original2};
+//   shallowCopy2.address.city = 'kangeyam';
+//   shallowCopy2.detail.name = 'kannan';
+
+//   console.log(original2.address.city);
+//   console.log(shallowCopy2.address.city);
+
+//   console.log(original2.detail.name);
+//   console.log(shallowCopy2.detail.name);
+   
+// deep copy
